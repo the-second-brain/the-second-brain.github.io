5fabe2bbdb5b76ef0e46640a68753bae8f8e1014
pskepp <peterskeppstedt@gmail.com>
2024-04-20 12:05:37 +0200

    New databse.

--- js/utils.js
+++ js/utils.js
@@ -18,7 +18,7 @@ function get_uid () {
 }
 
 async function loadAndMerge() {
-    let response = await fetch('json/saved_projects_v12.json');
+    let response = await fetch('json/saved_projects_v13.json');
 	let saved_cubes = await response.json();
 	let current_cubes = JSON.parse(localStorage.getItem("cubes"));
 	localStorage.setItem("cubes", JSON.stringify(current_cubes.concat(saved_cubes)));
